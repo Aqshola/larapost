d57ae0cbc29de91373cd0c2e91bb974acb4812fe
Aqshola <aqsholafid2@gmail.com>
2021-12-21 21:24:55 +0700

    init mini post, start create model

---
+++ resources/views/post.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.layout')
+
+@section('container')
+    <h1>Lalatina</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit natus placeat, et iste harum aut! Ad officiis quam,
+        tenetur natus rem excepturi molestiae explicabo molestias aut, minus est ut beatae corrupti enim quisquam deleniti
+        culpa ipsa saepe maxime. Quam, reprehenderit!</p>
+@endsection
